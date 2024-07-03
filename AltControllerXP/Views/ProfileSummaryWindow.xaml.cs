6de257b58c8a4a985f69e13d16bb1e29cae4344d
Garrett Anderson <gband85@mailfence.com>
2024-07-03 12:15:32 -0500

    add views

---
+++ AltControllerXP/Views/ProfileSummaryWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿/*
+Alt Controller
+--------------
+Copyright 2013 Tim Brogden
+http://altcontroller.net
+
+Description
+-----------
+A free program for mapping computer inputs, such as pointer movements and button presses, 
+to actions, such as key presses. The aim of this program is to help make third-party programs,
+such as computer games, more accessible to users with physical difficulties.
+
+License
+-------
+This file is part of Alt Controller. 
+Alt Controller is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Alt Controller is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Alt Controller.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Data;
+using AltController.Actions;
+using AltController.Config;
+using AltController.Core;
+
+namespace AltControllerX.Views
+{
+    /// <summary>
+    /// Window that shows a summary of the current profile
+    /// </summary>
+    public partial class ProfileSummaryWindow : Window
+    {
+        public ProfileSummaryWindow()
+        {
+            InitializeComponent();
+        }       
+    }
+}
