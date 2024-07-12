aaeef401542e1d564615f3996b39c2e9e62d31e3
Garrett Anderson <gband85@mailfence.com>
2024-07-12 12:07:33 -0500

    add code

--- AltControllerXP/Core/Utils.cs
+++ AltControllerXP/Core/Utils.cs
@@ -38,221 +38,221 @@ namespace AltControllerXP.Core
     /// </summary>
     public class Utils
     {                
-        ///// <summary>
-        ///// Get the name for each type of action
-        ///// </summary>
-        ///// <param name="actionType"></param>
-        ///// <returns></returns>
-        //public string GetActionTypeName(EActionType actionType)
-        //{
-        //    string name;
-        //    switch (actionType)
-        //    {
-        //        case EActionType.ChangeMode:
-        //            name = Properties.Resources.String_Change_mode; break;
-        //        case EActionType.ChangePage:
-        //            name = Properties.Resources.String_Change_page; break;
-        //        case EActionType.ChangePointer:
-        //            name = Properties.Resources.String_Change_pointer; break;
-        //        case EActionType.HoldKey:
-        //            name = Properties.Resources.String_Hold_key; break;
-        //        case EActionType.MouseClick:
-        //            name = Properties.Resources.String_Mouse_click; break;
-        //        case EActionType.MouseDoubleClick:
-        //            name = Properties.Resources.String_Mouse_double_click; break;
-        //        case EActionType.MouseHold:
-        //            name = Properties.Resources.String_Mouse_hold; break;
-        //        case EActionType.MouseRelease:
-        //            name = Properties.Resources.String_Mouse_release; break;
-        //        case EActionType.MoveThePointer:
-        //            name = Properties.Resources.String_Move_the_pointer; break;
-        //        case EActionType.ReleaseKey:
-        //            name = Properties.Resources.String_Release_key; break;
-        //        case EActionType.RepeatKey:
-        //            name = Properties.Resources.String_Repeat_key; break;
-        //        case EActionType.RepeatKeyDirectional:
-        //            name = Properties.Resources.String_Repeat_key_directional; break;
-        //        case EActionType.RepeatScrollDown:
-        //            name = Properties.Resources.String_Repeat_scroll_down; break;
-        //        case EActionType.RepeatScrollUp:
-        //            name = Properties.Resources.String_Repeat_scroll_up; break;
-        //        case EActionType.ScrollDown:
-        //            name = Properties.Resources.String_Scroll_down; break;
-        //        case EActionType.ScrollUp:
-        //            name = Properties.Resources.String_Scroll_up; break;
-        //        case EActionType.StopOngoingActions:
-        //            name = Properties.Resources.String_Stop_ongoing_actions; break;
-        //        case EActionType.StopScrolling:
-        //            name = Properties.Resources.String_Stop_scrolling; break;
-        //        case EActionType.ToggleKey:
-        //            name = Properties.Resources.String_Toggle_key; break;
-        //        case EActionType.ToggleMouseButton:
-        //            name = Properties.Resources.String_Toggle_mouse_button; break;
-        //        case EActionType.TypeKey:
-        //            name = Properties.Resources.String_Type_key; break;
-        //        case EActionType.TypeText:
-        //            name = Properties.Resources.String_Type_text; break;
-        //        case EActionType.Wait:
-        //            name = Properties.Resources.String_Wait; break;
-        //        default:
-        //            name = "N/K"; break;
-        //    }
+        /// <summary>
+        /// Get the name for each type of action
+        /// </summary>
+        /// <param name="actionType"></param>
+        /// <returns></returns>
+        public string GetActionTypeName(EActionType actionType)
+        {
+            string name;
+            switch (actionType)
+            {
+                case EActionType.ChangeMode:
+                    name = Properties.Resources.String_Change_mode; break;
+                case EActionType.ChangePage:
+                    name = Properties.Resources.String_Change_page; break;
+                case EActionType.ChangePointer:
+                    name = Properties.Resources.String_Change_pointer; break;
+                case EActionType.HoldKey:
+                    name = Properties.Resources.String_Hold_key; break;
+                case EActionType.MouseClick:
+                    name = Properties.Resources.String_Mouse_click; break;
+                case EActionType.MouseDoubleClick:
+                    name = Properties.Resources.String_Mouse_double_click; break;
+                case EActionType.MouseHold:
+                    name = Properties.Resources.String_Mouse_hold; break;
+                case EActionType.MouseRelease:
+                    name = Properties.Resources.String_Mouse_release; break;
+                case EActionType.MoveThePointer:
+                    name = Properties.Resources.String_Move_the_pointer; break;
+                case EActionType.ReleaseKey:
+                    name = Properties.Resources.String_Release_key; break;
+                case EActionType.RepeatKey:
+                    name = Properties.Resources.String_Repeat_key; break;
+                case EActionType.RepeatKeyDirectional:
+                    name = Properties.Resources.String_Repeat_key_directional; break;
+                case EActionType.RepeatScrollDown:
+                    name = Properties.Resources.String_Repeat_scroll_down; break;
+                case EActionType.RepeatScrollUp:
+                    name = Properties.Resources.String_Repeat_scroll_up; break;
+                case EActionType.ScrollDown:
+                    name = Properties.Resources.String_Scroll_down; break;
+                case EActionType.ScrollUp:
+                    name = Properties.Resources.String_Scroll_up; break;
+                case EActionType.StopOngoingActions:
+                    name = Properties.Resources.String_Stop_ongoing_actions; break;
+                case EActionType.StopScrolling:
+                    name = Properties.Resources.String_Stop_scrolling; break;
+                case EActionType.ToggleKey:
+                    name = Properties.Resources.String_Toggle_key; break;
+                case EActionType.ToggleMouseButton:
+                    name = Properties.Resources.String_Toggle_mouse_button; break;
+                case EActionType.TypeKey:
+                    name = Properties.Resources.String_Type_key; break;
+                case EActionType.TypeText:
+                    name = Properties.Resources.String_Type_text; break;
+                case EActionType.Wait:
+                    name = Properties.Resources.String_Wait; break;
+                default:
+                    name = "N/K"; break;
+            }
 
-        //    return name;
-        //}
+            return name;
+        }
 
-        //public string GetDisplayAreaName(EDisplayArea area)
-        //{
-        //    string name;
-        //    switch (area)
-        //    {
-        //        case EDisplayArea.PrimaryScreen:
-        //            name = Properties.Resources.String_Primary_screen;
-        //            break;
-        //        case EDisplayArea.PrimaryWorkingArea:
-        //            name = Properties.Resources.String_Primary_working_area;
-        //            break;
-        //        case EDisplayArea.WorkingArea:
-        //            name = Properties.Resources.String_Virtual_working_area;
-        //            break;
-        //        case EDisplayArea.ActiveWindow:
-        //            name = Properties.Resources.String_Active_window;
-        //            break;
-        //        case EDisplayArea.Desktop:
-        //        default:
-        //            name = Properties.Resources.String_Virtual_desktop;
-        //            break;
-        //    }
+        public string GetDisplayAreaName(EDisplayArea area)
+        {
+            string name;
+            switch (area)
+            {
+                case EDisplayArea.PrimaryScreen:
+                    name = Properties.Resources.String_Primary_screen;
+                    break;
+                case EDisplayArea.PrimaryWorkingArea:
+                    name = Properties.Resources.String_Primary_working_area;
+                    break;
+                case EDisplayArea.WorkingArea:
+                    name = Properties.Resources.String_Virtual_working_area;
+                    break;
+                case EDisplayArea.ActiveWindow:
+                    name = Properties.Resources.String_Active_window;
+                    break;
+                case EDisplayArea.Desktop:
+                default:
+                    name = Properties.Resources.String_Virtual_desktop;
+                    break;
+            }
 
-        //    return name;
-        //}
+            return name;
+        }
 
-        ///// <summary>
-        ///// Get the name for each type of input source
-        ///// </summary>
-        ///// <param name="sourceType"></param>
-        ///// <returns></returns>
-        //public string GetSourceTypeName(ESourceType sourceType)
-        //{
-        //    string name;
-        //    switch (sourceType)
-        //    {
-        //        case ESourceType.CustomWindow:
-        //            name = Properties.Resources.String_Custom_Window; break;
-        //        case ESourceType.Keyboard:
-        //            name = Properties.Resources.String_Keyboard; break;
-        //        case ESourceType.Mouse:
-        //            name = Properties.Resources.String_Mouse; break;
-        //        case ESourceType.Internal:
-        //            name = Properties.Resources.String_Internal; break;
-        //        default:
-        //            name = "N/K"; break;
-        //    }
+        /// <summary>
+        /// Get the name for each type of input source
+        /// </summary>
+        /// <param name="sourceType"></param>
+        /// <returns></returns>
+        public string GetSourceTypeName(ESourceType sourceType)
+        {
+            string name;
+            switch (sourceType)
+            {
+                case ESourceType.CustomWindow:
+                    name = Properties.Resources.String_Custom_Window; break;
+                case ESourceType.Keyboard:
+                    name = Properties.Resources.String_Keyboard; break;
+                case ESourceType.Mouse:
+                    name = Properties.Resources.String_Mouse; break;
+                case ESourceType.Internal:
+                    name = Properties.Resources.String_Internal; break;
+                default:
+                    name = "N/K"; break;
+            }
 
-        //    return name;
-        //}
+            return name;
+        }
 
-        ///// <summary>
-        ///// Shape to string
-        ///// </summary>
-        ///// <param name="shape"></param>
-        ///// <returns></returns>
-        //public string GetShapeName(EShape shape) 
-        //{
-        //    string name;
-        //    switch (shape)
-        //    {
-        //        case EShape.Annulus:
-        //            name = Properties.Resources.String_Annulus; break;
-        //        case EShape.AnnulusSector:
-        //            name = Properties.Resources.String_Annulus_sector; break;
-        //        case EShape.Ellipse:
-        //            name = Properties.Resources.String_Ellipse; break;
-        //        case EShape.EllipseSector:
-        //            name = Properties.Resources.String_Ellipse_sector; break;
-        //        case EShape.Rectangle:
-        //            name = Properties.Resources.String_Rectangle; break;
-        //        default:
-        //            name = "N/K"; break;
-        //    }
+        /// <summary>
+        /// Shape to string
+        /// </summary>
+        /// <param name="shape"></param>
+        /// <returns></returns>
+        public string GetShapeName(EShape shape) 
+        {
+            string name;
+            switch (shape)
+            {
+                case EShape.Annulus:
+                    name = Properties.Resources.String_Annulus; break;
+                case EShape.AnnulusSector:
+                    name = Properties.Resources.String_Annulus_sector; break;
+                case EShape.Ellipse:
+                    name = Properties.Resources.String_Ellipse; break;
+                case EShape.EllipseSector:
+                    name = Properties.Resources.String_Ellipse_sector; break;
+                case EShape.Rectangle:
+                    name = Properties.Resources.String_Rectangle; break;
+                default:
+                    name = "N/K"; break;
+            }
 
-        //    return name;
-        //}
+            return name;
+        }
 
-        ///// <summary>
-        ///// Reason to string
-        ///// </summary>
-        ///// <param name="eventReason"></param>
-        ///// <returns></returns>
-        //public string GetReasonName(EEventReason reason)
-        //{
-        //    string name;
-        //    switch (reason)
-        //    {
-        //        case EEventReason.Pressed:
-        //            name = Properties.Resources.String_Pressed; break;
-        //        case EEventReason.Released:
-        //            name = Properties.Resources.String_Released; break;
-        //        case EEventReason.Dwelled:
-        //            name = Properties.Resources.String_Dwelled; break;
-        //        case EEventReason.Inside:
-        //            name = Properties.Resources.String_Inside; break;
-        //        case EEventReason.Outside:
-        //            name = Properties.Resources.String_Outside; break;
-        //        case EEventReason.Moved:
-        //            name = Properties.Resources.String_Moved; break;
-        //        case EEventReason.Updated:
-        //            name = Properties.Resources.String_Updated; break;
-        //        default:
-        //            name = Properties.Resources.String_None; break;
-        //    }
-        //    return name;
-        //}
+        /// <summary>
+        /// Reason to string
+        /// </summary>
+        /// <param name="eventReason"></param>
+        /// <returns></returns>
+        public string GetReasonName(EEventReason reason)
+        {
+            string name;
+            switch (reason)
+            {
+                case EEventReason.Pressed:
+                    name = Properties.Resources.String_Pressed; break;
+                case EEventReason.Released:
+                    name = Properties.Resources.String_Released; break;
+                case EEventReason.Dwelled:
+                    name = Properties.Resources.String_Dwelled; break;
+                case EEventReason.Inside:
+                    name = Properties.Resources.String_Inside; break;
+                case EEventReason.Outside:
+                    name = Properties.Resources.String_Outside; break;
+                case EEventReason.Moved:
+                    name = Properties.Resources.String_Moved; break;
+                case EEventReason.Updated:
+                    name = Properties.Resources.String_Updated; break;
+                default:
+                    name = Properties.Resources.String_None; break;
+            }
+            return name;
+        }
 
-        ///// <summary>
-        ///// Mouse button to string
-        ///// </summary>
-        ///// <param name="button"></param>
-        ///// <returns></returns>
-        //public string GetMouseButtonName(EMouseButton button)
-        //{
-        //    string name;
-        //    switch (button) 
-        //    {
-        //        case EMouseButton.Left:
-        //            name = Properties.Resources.String_Left; break;
-        //        case EMouseButton.Middle:
-        //            name = Properties.Resources.String_Middle; break;
-        //        case EMouseButton.Right:
-        //            name = Properties.Resources.String_Right; break;
-        //        case EMouseButton.X1:
-        //            name = Properties.Resources.String_X1; break;
-        //        case EMouseButton.X2:
-        //            name = Properties.Resources.String_X2; break;
-        //        default:
-        //            name = "N/K"; break;
-        //    }
+        /// <summary>
+        /// Mouse button to string
+        /// </summary>
+        /// <param name="button"></param>
+        /// <returns></returns>
+        public string GetMouseButtonName(EMouseButton button)
+        {
+            string name;
+            switch (button) 
+            {
+                case EMouseButton.Left:
+                    name = Properties.Resources.String_Left; break;
+                case EMouseButton.Middle:
+                    name = Properties.Resources.String_Middle; break;
+                case EMouseButton.Right:
+                    name = Properties.Resources.String_Right; break;
+                case EMouseButton.X1:
+                    name = Properties.Resources.String_X1; break;
+                case EMouseButton.X2:
+                    name = Properties.Resources.String_X2; break;
+                default:
+                    name = "N/K"; break;
+            }
 
-        //    return name;
-        //}        
+            return name;
+        }        
 
-        ///// <summary>
-        ///// Return whether a parameter type conversion is possible
-        ///// </summary>
-        ///// <param name="fromType"></param>
-        ///// <param name="toType"></param>
-        ///// <returns></returns>
-        //public bool IsDataTypeConversionValid(EDataType fromType, EDataType toType)
-        //{
-        //    bool isValid = false;
+        /// <summary>
+        /// Return whether a parameter type conversion is possible
+        /// </summary>
+        /// <param name="fromType"></param>
+        /// <param name="toType"></param>
+        /// <returns></returns>
+        public bool IsDataTypeConversionValid(EDataType fromType, EDataType toType)
+        {
+            bool isValid = false;
             
-        //    if (fromType == toType)
-        //    {
-        //        isValid = true;
-        //    }
+            if (fromType == toType)
+            {
+                isValid = true;
+            }
 
-        //    return isValid;
-        //}
+            return isValid;
+        }
         public void OpenUrl(string url)
         {
             try
